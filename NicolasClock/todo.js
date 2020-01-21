3ef5907e4e7d103b2563de5ef3ca8a552c54e10a
Nohel <ckddhr7560@naver.com>
2020-01-21 21:47:49 +0900

    remove toDoList

--- NicolasClock/todo.js
+++ NicolasClock/todo.js
@@ -1,15 +1,22 @@
 const toDoForm = document.querySelector(".js-toDoForm"),
     toDoInput = toDoForm.querySelector("input"),
-    todoList = document.querySelector(".js-toDoList");
+    toDoList = document.querySelector(".js-toDoList");
 
 const TODOS_LS = "toDos";
 
+
 //할일을 저장하는 List를 만듬.
-const toDos = [];
+let toDos = [];
 
 function deleteToDo(event){
     const btn = event.target;
     const li = btn.parentNode;
+    toDoList.removeChild(li);
+    const cleanToDos = toDos.filter(function(toDo) {
+        return toDo.id !== parseInt(li.id);
+    });
+    toDos = cleanToDos;
+    saveToDos();
 }
 
 function saveToDos(){
@@ -23,14 +30,14 @@ function paintToDo(text){
     const li = document.createElement("li");
     const delBtn = document.createElement("button");
     const span = document.createElement("span");
-    const newId = toDos.length +1;
+    const newId = toDos.length + 1;
     delBtn.innerHTML = "x";
     delBtn.addEventListener("click", deleteToDo);
     span.innerText = text;
     li.appendChild(delBtn); //li에 delBtn 버튼추가.
     li.appendChild(span); //li에 <span>추가.
     li.id = newId;
-    todoList.appendChild(li); // ul에 li추가.
+    toDoList.appendChild(li); // ul에 li추가.
     
     const toDoObj = {
         text: text,
