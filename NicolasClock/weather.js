6473a38d5b5da5b9f7b676b96c813c3ffb9f6d1d
Nohel <ckddhr7560@naver.com>
2020-01-27 23:06:34 +0900

    update weather.js(completion)

--- NicolasClock/weather.js
+++ NicolasClock/weather.js
@@ -1,12 +1,19 @@
+const weather = document.querySelector(".js-weather");
+
 const API_KEY = "9a94b0b320b5d82ce1642141fd260dc7";
 const COORDS = 'coords';
 
-function getWeather(lat, lng){
+function getWeather(lat, lon){
     fetch(
         `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
-    ).then(function(json){
-        console.log(json);
-    }); //데이터가 완전히 넘어온다음 호출한다.
+    ).then(function(response){
+        return response.json()
+    }).then(function(Rsjson){
+        console.log(Rsjson)
+        const temperature = Rsjson.main.temp;
+        const place = Rsjson.name;
+        weather.innerText =`${temperature} @ ${place}`;
+    }) //then()은 데이터가 완전히 넘어온다음 호출한다.
 }
 
 //위도와 경도를 localStorage에 저장한다.  
@@ -23,7 +30,7 @@ function handleGeoSucces(position){
         longitude    //longitude : longitude
     };
     saveCoords(coordsObj);
-    getWeather(latitude, longitude);
+    getWeather(latitude, longitude)
 }
 
 function handleGeoError(){
@@ -41,7 +48,7 @@ function loadCoords(){
         askForCoords();
     }else{
         const parseCoords = JSON.parse(loadedCoords);
-        getWeather(parseCoords.latitude, parseCoords.longitude); 
+         getWeather(parseCoords.latitude, parseCoords.longitude); 
     }
 }
 
